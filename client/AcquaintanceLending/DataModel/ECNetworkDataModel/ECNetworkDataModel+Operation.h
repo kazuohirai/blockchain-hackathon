b9e78f44a1a8ef464d2c798e727e348f747bde73
Sola Li <tscc01@163.com>
2016-01-09 19:23:50 +0800

    add iOS client part

---
+++ client/AcquaintanceLending/DataModel/ECNetworkDataModel/ECNetworkDataModel+Operation.h
@@ -0,0 +1,31 @@
+//
+//  ECNetworkDataModel+Operation.h
+//  ECarDriver
+//
+//  Created by sola on 15/8/18.
+//  Copyright (c) 2015年 upluscar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "ECNetworkDataModel.h"
+
+@interface ECNetworkDataModel (Operation)
+
+- (void)getBorrowInfoWithSuccess:(void (^)(AFHTTPRequestOperation *operation, id responseObject))success failure:(void (^)(AFHTTPRequestOperation *operation, NSError *error))failure;
+
+- (void)getBorrowingInfoWithSuccess:(void (^)(AFHTTPRequestOperation *operation, id responseObject))success failure:(void (^)(AFHTTPRequestOperation *operation, NSError *error))failure;
+
+- (void)getLendInfoWithSuccess:(void (^)(AFHTTPRequestOperation *operation, id responseObject))success failure:(void (^)(AFHTTPRequestOperation *operation, NSError *error))failure;
+
+- (void)getLendingInfoWithSuccess:(void (^)(AFHTTPRequestOperation *operation, id responseObject))success failure:(void (^)(AFHTTPRequestOperation *operation, NSError *error))failure;
+
+- (void)getDoneInfoWithSuccess:(void (^)(AFHTTPRequestOperation *operation, id responseObject))success failure:(void (^)(AFHTTPRequestOperation *operation, NSError *error))failure;
+
+
+- (void)requestForNewBorrow:(NSString*)strAmount profit:(NSString*)strProfit repayDate:(NSString*)strRepayDate withSuccess:(void (^)(AFHTTPRequestOperation *operation, id responseObject))success failure:(void (^)(AFHTTPRequestOperation *operation, NSError *error))failure;
+
+- (void)requestForLend:(NSString*)strOrder withSuccess:(void (^)(AFHTTPRequestOperation *operation, id responseObject))success failure:(void (^)(AFHTTPRequestOperation *operation, NSError *error))failure;
+
+- (void)requestForRepay:(NSString*)strOrder withSuccess:(void (^)(AFHTTPRequestOperation *operation, id responseObject))success failure:(void (^)(AFHTTPRequestOperation *operation, NSError *error))failure;
+
+@end
