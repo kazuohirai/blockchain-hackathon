bd3a8e8ce0e40ff71a3ada9a60982ba9c8e01927
yann <yann@yanndeMacBook-Pro-2.local>
2016-01-09 10:26:18 +0800

    add trade controller

--- server/src/main/java/org/blockchain/borrowing/web/TradeController.java
+++ server/src/main/java/org/blockchain/borrowing/web/TradeController.java
@@ -1,11 +1,64 @@
 package org.blockchain.borrowing.web;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.apache.log4j.Logger;
+import org.blockchain.borrowing.domain.Trade;
+import org.blockchain.borrowing.service.TradeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
-@RequestMapping("/transaction")
+@RequestMapping("/user/{userId}/trade")
 public class TradeController {
 
+    private static final Logger LOG = Logger.getLogger(TradeController.class);
 
+    @Autowired
+    private TradeService tradeService;
+
+    /**
+     * query all trades
+     *
+     * @param userId
+     * @return
+     */
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Trade> list(@PathVariable("userId") long userId,
+                            @RequestParam(value = "staus", required = false, defaultValue = "ING") String status) {
+        return Arrays.asList(Trade.sampleValue(), Trade.sampleValue(), Trade.sampleValue());
+    }
+
+    /**
+     * Create a trade
+     *
+     * @param userId
+     * @param trade
+     * @return
+     */
+    @RequestMapping(method = RequestMethod.POST)
+    public Trade create(@PathVariable("userId") long userId, @RequestBody Trade trade) {
+        return Trade.sampleValue();
+    }
+
+    /**
+     * Get a detail of trade
+     *
+     * @param userId
+     * @param tradeNo
+     * @return
+     */
+    @RequestMapping(path = "/{tradeNo}", method = RequestMethod.GET)
+    public Trade get(@PathVariable("userId") long userId, @PathVariable("tradeNo") String tradeNo) {
+        return Trade.sampleValue();
+    }
+
+
+    @RequestMapping(path = "/{tradeNo}", method = RequestMethod.POST)
+    public Trade update(@PathVariable("userId") long userId,
+                        @PathVariable("tradeNo") String tradeNo,
+                        @RequestParam(value = "staus", required = false, defaultValue = "ING") String status) { /* ING 借钱, COM 还钱 */
+        return Trade.sampleValue();
+    }
 }
